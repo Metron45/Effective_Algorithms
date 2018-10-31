f4c557fa117107d7cccc59247db882e21b2d578f
Marek Chmielewski <Metron45@gmail.com>
2018-10-31 12:18:00 +0100

    Dynamiczne v0.2
    Pozostaje obsługa błędu gry nie znajdzie kolejnego wierzchołka ponieważ wszystkie ścieżki zawierają już obecny

--- Projektowanie_Efektywnych_Algorytmow/Dynamic.cpp
+++ Projektowanie_Efektywnych_Algorytmow/Dynamic.cpp
@@ -16,30 +16,106 @@ Dynamic::~Dynamic()
 
 void Dynamic::initialize_route_table() 
 {
-	route_table = new int *[distance_table_size - 1];
-	for (int i = 0; i < distance_table_size - 1; i++) {
+	route_table = new int *[distance_table_size-1];
+	for (int i = 0; i < distance_table_size-1; i++) {
 		route_table[i] = new int[distance_table_size];
 	}
+	initialize_route_table_distance();
 }
 
 void Dynamic::initialize_route_table_distance()
 {
 	for (int i = 0; i < distance_table_size - 1; i++) {
-		distance_table[i][1] = i+1;
-		distance_table[i][0] = get_distance(0, i + 1);
+		route_table[i][1] = i+1;
+		route_table[i][0] = get_distance(0, i + 1);
+		for (int y = 2; y < distance_table_size; y++) {
+			route_table[i][y] = -1;
+		}
+	}
+}
+
+bool Dynamic::check_visited(int city, int route) {
+	int route_position = 1;
+	while (route_position < distance_table_size ) {
+		if (city == route_table[route][route_position]) {
+			return false;
+		} 
+		else if (route_table[route][route_position] <= 0) {
+			return true;
+		}
+		route_position++;
+	}
+	return true;
+}
+
+void Dynamic::redo_routes(int * new_route, int route_iteration) {
+	int ** new_route_table = new int *[distance_table_size - 1];
+	for (int line = 0; line < distance_table_size - 1; line++) {
+		new_route_table[line] = new int[distance_table_size-1];
+		for (int column = 0; column < distance_table_size - 1; column++) {
+			new_route_table[line][column] = route_table[new_route[line]][column + 1];
+		}
+		new_route_table[line][route_iteration-1] = line + 1;
+	}
+
+	for (int line = 0; line < distance_table_size - 1; line++) {
+		for (int column = 0; column < distance_table_size - 1; column++) {
+			route_table[line][column + 1]= new_route_table[line][column];
+		}
 	}
 }
 
 int Dynamic::dynamic()
 {
+	//for S only having 2 elements
 	initialize_route_table();
-	initialize_route_table_distance();
-	for (int i = 2; i < distance_table_size; i++) {
-
+	draw_route_table();
+	//for S having over 2 elements 
+	int *new_distance, *new_route;
+	for (int route_iteration = 2; route_iteration < 4; route_iteration++) {
+		new_distance =	new int[distance_table_size - 1];
+		new_route =		new int[distance_table_size - 1];
+		for (int route = 0; route < distance_table_size - 1; route++) {
+			new_distance[route] = 0;
+			//znalezienie min
+			for (int route_min = 0; route_min < distance_table_size - 1; route_min++) {
+				if (check_visited(route + 1, route_min)) {
+					if (new_distance[route] == 0) {
+						new_distance[route] = get_distance(route_table[route_min][route_iteration - 1], route + 1);
+						new_route[route] = route_min;
+					}
+					else if (route_table[route_min][0] + get_distance(route_table[route_min][route_iteration - 1], route + 1) < new_distance[route]) {
+						new_distance[route] = get_distance(route_table[route_min][route_iteration - 1], route + 1);
+						new_route[route] = route_min;
+					}
+				}
+			}
+			if (new_distance[route] = 0) {
+				//wykonanie operacji								------------------------------------------------------
+			}
+			std::cout << "min: " << std::setw(3) << route <<
+				std::setw(3) << new_distance[route] <<
+				std::setw(3) << new_route[route] << std::endl;
+		}
+		for (int route = 0; route < distance_table_size - 1; route++) {
+			route_table[route][0] += new_distance[route];
+		}
+		redo_routes(new_route, route_iteration);
 	}
-
 	return 0;
 }
+
+void Dynamic::draw_route_table() {
+	std::cout <<"Route Table for dynamic programming" << std::endl;
+	for (int line = 0; line < distance_table_size-1; line++) {
+		std::cout << line << ": " ;
+		for (int column = 0; column < distance_table_size; column++) {
+			std::cout << std::setw(4) << route_table[line][column];
+		}
+		std::cout << std::endl;
+	}
+}
+
 
 int Dynamic::dynamic(int * S, int sizeS, int i)
 {
--- Projektowanie_Efektywnych_Algorytmow/Dynamic.h
+++ Projektowanie_Efektywnych_Algorytmow/Dynamic.h
@@ -10,7 +10,10 @@ public:
 	~Dynamic();
 	void initialize_route_table();
 	void initialize_route_table_distance();
+	bool check_visited(int city, int route);
+	void redo_routes(int * new_route, int route_iteration);
 	int dynamic();
+	void draw_route_table();
 	int dynamic(int * S, int sizeS, int i);
 };
 
